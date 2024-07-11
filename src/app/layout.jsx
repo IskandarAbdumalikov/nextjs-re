d2a82be72b7275aeadadc2034457b0adc615824f
iskandarprogrammer2007 <abdumalikoviskandar600@gmail.com>
2024-07-11 08:42:43 +0500

    added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header/Header";
+import StoreProvider from "./StoreProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Header />
-        {children}
-      </body>
+      <StoreProvider>
+        <body className={inter.className}>
+          <Header />
+          {children}
+        </body>
+      </StoreProvider>
     </html>
   );
 }
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,6 +1,11 @@
+'use client'
+import { decrement, increment } from "@/lib/features/counter/counterSlice";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const Hero = () => {
+  let dispatch = useDispatch();
+  let count = useSelector((state) => state.counter.value);
   return (
     <div
       className="bg-no-repeat bg-center bg-cover p-[180px] flex items-center justify-center  pt-[200px]"
@@ -16,9 +21,24 @@ const Hero = () => {
         <h2 style={{ fontSize: 35 }}>The nature candle</h2>
         <h3>
           All handmade with natural soy wax, Candleaf is a companion for all
-          your pleasure moments{" "}
+          your pleasure moments
         </h3>
-        <button>Discovery our collection</button>
+        <div className="counter flex items-center gap-4">
+          <button
+           disabled={count<=0}
+            onClick={() => dispatch(decrement())}
+            className="px-4 py-2 bg-gray-200 rounded disabled:opacity-50"
+          >
+            -
+          </button>
+          <span className="text-2xl">{count}</span>
+          <button
+            onClick={() => dispatch(increment())}
+            className="px-4 py-2 bg-gray-200 rounded"
+          >
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
